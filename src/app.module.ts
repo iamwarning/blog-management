92d33737e18ee3f3a19557c297abdb9300f81b2f
jorgel <siul.gomez94@gmail.com>
2023-02-10 23:41:59 -0600

    :sparkles: Add MongoDB Atlas connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(`${process.env.MONGO_URI}`, {
+      connectionName: process.env.MONGO_CONNECTION_NAME,
+      dbName: process.env.MONGO_DATABASE,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
